9bd4becb12a3a195a3f061e9608810a9ae4b7a42
NicKomarov <mykyta.komarov73@gmail.com>
2022-10-11 23:23:01 +0300

    Add project files.

---
+++ AbsClass/Program.cs
@@ -0,0 +1,25 @@
+﻿
+abstract class GeographicObject
+{ 
+    public decimal CoordX, CoordY;
+    public string Name, Description;
+    public virtual void GetInfo() { }
+}
+
+class River : GeographicObject
+{
+    public decimal Speed, Length;
+}
+
+class Mountain : GeographicObject
+{
+    public decimal HighestPoint;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Itnerface/Program.cs
@@ -0,0 +1,46 @@
+﻿
+interface IGeographicObject
+{
+    decimal CoordX { get; set; }
+    decimal CoordY { get; set; }
+    string Name { get; set; }
+    string Description { get; set; }
+
+    void GetInfo();
+}
+
+class River : IGeographicObject
+{
+    public decimal Speed, Length;
+    public decimal CoordX { get; set; }
+    public decimal CoordY { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public void GetInfo()
+    {
+        Console.WriteLine($"Назва: {Name}, Опис: {Description}, координата х: {CoordX}, координата y: {CoordY} ");
+    }
+}
+
+class Mountain : IGeographicObject
+{
+    public decimal HighestPoint;
+    public decimal CoordX { get; set; }
+    public decimal CoordY { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public void GetInfo()
+    {
+        Console.WriteLine($"Назва: {Name}, Опис: {Description}, координата х: {CoordX}, координата y: {CoordY} ");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
